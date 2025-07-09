6b25569af973baca81fd659275c13c7cf9998e12
Kjetil Klaussen <kjetil.klaussen@gmail.com>
2025-07-09 13:44:14 +0200

    chore: Added Vercel speed insights (perf monitoring)

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,5 @@
 <script lang="ts">
 	import '../app.css';
-	import { dev } from '$app/environment';
-	import { injectAnalytics } from '@vercel/analytics/sveltekit';
-
-	injectAnalytics({ mode: dev ? 'development' : 'production' });
 	
 	let { children } = $props();
 
